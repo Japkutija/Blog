1a625bc1aa9923a21034aedc72d11dbf34fb5058
Jasmin Dervisevic <jasko.derv@gmail.com>
2024-02-16 23:05:17 +0100

    - Added Swagger API documentation for Post Controller.

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -4,7 +4,10 @@ import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.service.PostService;
 import com.springboot.blog.utils.AppConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -24,10 +27,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/posts")
+@Tag(
+        name = "CRUD Operations on Posts",
+        description = "Post API"
+)
 public class PostController {
 
     private final PostService postService;
 
+    @Operation(
+            summary = "Get all posts REST API",
+            description = "Get all posts with pagination and sorting"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Posts retrieved successfully"
+    )
     @GetMapping
     public PostResponse getAllPosts(
             @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required =
@@ -38,6 +53,14 @@ public class PostController {
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
+    @Operation(
+            summary = "Create a new post REST API",
+            description = "Create a new post"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Post created successfully"
+    )
     @SecurityRequirement(
             name = "Bearer Authentication"
     )
@@ -47,11 +70,27 @@ public class PostController {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get a post by its ID REST API",
+            description = "Get a post by its ID"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Post retrieved successfully"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") Long id) {
         return new ResponseEntity<>(postService.getPostById(id), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Update a post by its ID REST API",
+            description = "Update a post by its ID"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Post updated successfully"
+    )
     @SecurityRequirement(
             name = "Bearer Authentication"
     )
@@ -61,6 +100,14 @@ public class PostController {
         return new ResponseEntity<>(postService.updatePost(postDto, id), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Delete a post by its ID REST API",
+            description = "Delete a post by its ID"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Post deleted successfully"
+    )
     @SecurityRequirement(
             name = "Bearer Authentication"
     )
@@ -72,6 +119,14 @@ public class PostController {
                 new ResponseEntity<>("Post not found", HttpStatus.NOT_FOUND);
     }
 
+    @Operation(
+            summary = "Get all posts by category REST API",
+            description = "Get all posts by category with pagination and sorting"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Posts retrieved successfully"
+    )
     @GetMapping("/category/{id}")
     public ResponseEntity<List<PostDto>> getPostsByCategory(@PathVariable(name = "id") Long categoryId) {
         return new ResponseEntity<>(postService.getPostsByCategory(categoryId), HttpStatus.OK);
