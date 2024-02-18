75090db3a932d29b1b7ad3a1be23f08e08142231
Jasmin Dervisevic <jasko.derv@gmail.com>
2024-02-18 18:28:32 +0100

    - Setup the database info to connect to for the prod environment.

--- src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -1,16 +1,21 @@
 package com.springboot.blog;
 
+import com.springboot.blog.entity.Role;
+import com.springboot.blog.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@RequiredArgsConstructor
 @SpringBootApplication
 @OpenAPIDefinition(
         info = @Info
@@ -34,7 +39,7 @@ import org.springframework.context.annotation.Bean;
         )
 
 )
-public class SpringbootBlogRestApiApplication {
+public class SpringbootBlogRestApiApplication implements CommandLineRunner {
 
     @Bean
     public ModelMapper modelMapper() {
@@ -45,5 +50,18 @@ public class SpringbootBlogRestApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
+    }
+
+    private final RoleRepository roleRepository;
+    
+    @Override
+    public void run(String... args) throws Exception {
+        Role adminRole = new Role();
+        adminRole.setName("ROLE_ADMIN");
+        roleRepository.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setName("ROLE_USER");
+        roleRepository.save(userRole);
     }
 }
